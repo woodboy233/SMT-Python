94adc303ef155d2f679d122e42078c4e96d41ab2
woodboy233 <pluto125@proton.me>
2025-07-31 11:13:47 +0800

    Add files via upload

---
+++ session 3 worksheet activites.py
@@ -0,0 +1,38 @@
+#username = input("Enter Username:")
+#print("Username: " + username)
+
+#input_text=input("Enter x: ")
+#x=int(input_text)
+
+#Task 1
+    #x=int(input("Enter x: "))
+    #y=int(input("Enter y: "))
+    #sum=x+y
+    #print(f"Addition of {x} and {y} is {sum}")
+
+#Task 2
+    #first_name =input("Enter your first name:")
+    #last_name=input("Enter your last name: ")
+    #print("Hello, "+ first_name + last_name)
+
+#Task 3
+    #first_num=float(input("First number:"))
+    #second_num=float(input("Second number:"))
+    #print("Maximum of these two numbers is:",max(first_num,second_num))
+
+#Task 4
+x=5
+y=6
+z=2
+print(x>y)
+print(x<=y)
+print(7<=x)
+print(x>y>z)
+print(x<y)
+print(y<x)
+print(x==y)
+print(x==20)
+print(x!=y)
+print(15!=x)
+print(x>=y)
+print(10>=x)
