38a9f9a8569db5410efc62d2bc27ad2bf6935f73
woodboy233 <pluto125@proton.me>
2025-07-31 11:13:13 +0800

    Add files via upload

---
+++ session2 powerpoint activities.py
@@ -0,0 +1,11 @@
+
+#activity 1
+#print("sep and end are","keyword arguements.",sep=" = ",end="!!!")
+
+#activity 2
+'''print("""
+___ ____ ___ ___
+ | |___ [___  |
+ | |____ ___] |""")'''  #triple quotation marks are a comment over multiple lines
+
+#activity 3
