342830f1fc470c713981edeb43868ff69331bcae
woodboy233 <pluto125@proton.me>
2025-07-31 11:15:59 +0800

    Add files via upload

---
+++ Session 4 worksheet tasks.py
@@ -0,0 +1,25 @@
+#task 1
+'''str_text="Python"
+print(str_text.center(30))'''
+
+#task 2
+'''str_text="Python!"
+print(str_text.endswith("!"))'''
+
+#Task 3
+'''str_text=" Python "
+print(str_text.lstrip())'''
+
+#task 4
+'''str_text="23 Smith Street"
+print(str_text.startswith("23"))'''
+
+#task 5
+'''str_text="23 Smith Street"
+print(str_text.upper())'''
+
+#task 6
+str_text="23 Smith Street"
+print(str_text.lower())
+
+
