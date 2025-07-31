e4eba785af5d9931273d28a1cf59ab36eedd2f18
woodboy233 <pluto125@proton.me>
2025-07-31 11:15:07 +0800

    Add files via upload

---
+++ Session 4 powerpoint activities.py
@@ -0,0 +1,51 @@
+
+#Task 1
+'''import string
+print("ASCII letters are:",string.ascii_letters)
+print("Printable characters are:",string.printable)'''
+
+#task 2
+'''print(ord('α'))
+print(chr(945))
+print(ord('ʊ'))
+print(ord('ʌ'))
+print(ord('©'))
+print(ord('😓'))'''
+
+#task 3
+'''letters="abcdefgABDZ"
+print("f" in letters)
+print("F" in letters)
+print("5" in letters)
+print("gx"not in letters)
+print("AB"in letters)'''
+
+#task 4
+'''letters="abcdefgABDZ"
+print(min(letters))
+print(min(letters[6:]))
+print(max(letters))
+print(min(letters[5:7]))'''
+
+#task 5
+'''letters="aaabcdefgABDZ"
+print(letters.count("a"))
+print(letters.capitalize())
+print(letters.find("AB"))
+print(letters.isalpha())
+print(letters.isdigit())
+print(letters.islower())
+print(letters.isupper())'''
+
+#task 6
+'''print("Python two".replace("two","three"))
+str_text="Word of python"
+print("Before replacement:",str_text)
+new_text=str_text.replace("python","Python")
+print("After:",new_text)
+str_text="two two Python"
+print("New text:",str_text)
+new_text=str_text.replace("two","three",2)
+print(new_text)'''
+
+
